d5485de380cd6fd727218e503e330b49dfd5ae7f
luismvargasg <1610@holbertonschool.com>
2022-05-20 21:14:56 -0500

    fixing format ESLint

--- routes/index.js
+++ routes/index.js
@@ -7,12 +7,8 @@ const router = (app) => {
   app.use(express.json());
   app.use('/', paths);
 
-  paths.get('/status', (request, response) =>
-    AppController.getStatus(request, response)
-  );
-  paths.get('/stats', (request, response) =>
-    AppController.getStats(request, response)
-  );
+  paths.get('/status', ((request, response) => AppController.getStatus(request, response)));
+  paths.get('/stats', ((request, response) => AppController.getStats(request, response)));
 };
 
 export default router;
