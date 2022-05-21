393cd443becc08748be25c84c21e19a146422599
luismvargasg <1610@holbertonschool.com>
2022-05-20 21:45:35 -0500

    adding task 4

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -1,6 +1,8 @@
 import sha1 from 'sha1';
 import DBClient from '../utils/db';
+import RedisClient from '../utils/redis';
 
+const { ObjectId } = require('mongodb');
 const Bull = require('bull');
 
 class UsersController {
